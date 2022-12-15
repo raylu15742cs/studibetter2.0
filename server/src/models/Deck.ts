22231503684c55f992ffd6eb5b3451ab0893d428
raylu15742cs <raylucs15742@gmail.com>
2022-12-15 14:23:11 -0800

    update deck

--- server/src/models/Deck.ts
+++ server/src/models/Deck.ts
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId
 
-const Deck = new Schema({
+const DeckSchema = new Schema({
     title: String
 })
+
+const DeckModel = mongoose.model("Deck", DeckSchema)
+
+export default DeckModel;
