b56517c40f9e3b1373edee6c84959e54f131fc13
raylu15742cs <raylucs15742@gmail.com>
2022-12-15 14:22:04 -0800

    edit deck

--- server/src/models/Deck.ts
+++ server/src/models/Deck.ts
@@ -3,9 +3,6 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId
 
-const BlogPost = new Schema({
-    author: ObjectId,
-    title: String,
-    body: String,
-    date: Date,
+const Deck = new Schema({
+    title: String
 })
