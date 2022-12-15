1586f50df0cb6abdeb320cd4ca58e30a9760bbed
raylu15742cs <raylucs15742@gmail.com>
2022-12-15 14:10:14 -0800

    add nodemon

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express , {Request, Response} from "express";
 
 const app = express()
 
-app.get("/", (req,res) => {
+app.get("/", (req: Request ,res: Response) => {
     res.send("hello world")
 })
 
