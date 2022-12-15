b3fa4a76a68941c34e475af1209bb0ed31bebc37
raylu15742cs <raylucs15742@gmail.com>
2022-12-15 14:35:01 -0800

    connect to mongoose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,25 @@
 import express , {Request, Response} from "express";
 import mongoose from "mongoose";
 
-const app = express()
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const db = mongoose.connect(`mongodb+srv://admin:${process.env.DB_Password}@cluster0.titas9x.mongodb.net/?retryWrites=true&w=majority`)
+import Deck from "./models/Deck"
+
+const PORT = 3001;
+
+const app = express()
 
 app.get("/", (req: Request ,res: Response) => {
     res.send("hello world")
 })
 
 app.listen(3000);
+
+mongoose
+    .connect(
+        `mongodb+srv://admin:${process.env.DB_Password}@cluster0.titas9x.mongodb.net/?retryWrites=true&w=majority`
+    ).then(() => {
+        console.log(`listing on port ${PORT}`)
+        app.listen(PORT)
+    })
