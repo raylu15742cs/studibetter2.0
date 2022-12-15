0aeec7e7fa80dcd1804e6185cb6da8b7894ed1d8
raylu15742cs <raylucs15742@gmail.com>
2022-12-15 14:12:23 -0800

    add mongoose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express , {Request, Response} from "express";
+import mongoose from "mongoose";
 
 const app = express()
+
+
 
 app.get("/", (req: Request ,res: Response) => {
     res.send("hello world")
