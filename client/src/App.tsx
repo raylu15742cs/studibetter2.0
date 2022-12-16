f11c6c532cda19ea481f2dcf04afd8ff14eb82cc
raylu15742cs <raylucs15742@gmail.com>
2022-12-15 20:33:15 -0800

    add delete cards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   async function handleCreateDeck(e: React.FormEvent) {
     e.preventDefault();
-    await fetch('http://localhost:5003/decks', {
+    const response = await fetch('http://localhost:5003/decks', {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -20,8 +20,17 @@ function App() {
       body:JSON.stringify({
         title,
       })
-    })
+    });
+    const deck = await response.json()
+    setDecks([...decks, deck])
     setTitle("")
+  }
+
+  async function handleDeleteDeck(deckId: string) {
+    await fetch(`http://localhost:5003/decks/${deckId}`, {
+      method: "DELETE",
+    })
+    setDecks(decks.filter((deck) => deck._id !==deckId))
   }
 
   useEffect(() => {
@@ -38,7 +47,9 @@ function App() {
     <div className="App">
       <ul className="decks">
         {decks.map((deck) => (
-            <li key={deck._id}>{deck.title}</li>
+            <li key={deck._id}>
+              <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
+              {deck.title}</li>
         ))
         }
       </ul>
--- server/src/index.ts
+++ server/src/index.ts
@@ -33,6 +33,15 @@ app.post("/decks" , async(req: Request, res: Response) => {
     res.json(createdDeck)
 })
 
+app.delete('/decks/:deckId', async(req: Request, res: Response) => {
+    //TODO
+    // 1. Get the deck id from the URL
+    const deckId = req.params.deckId
+    // 2. delete the deck from mongo
+    const deck = await Deck.findByIdAndDelete(deckId)
+    // 3. return the deleted deck to the user who made the request
+    res.json(deck)
+})
 mongoose
     .connect(process.env.MONGO_URL!).then(() => {
         console.log(`listing on port ${PORT}`)
