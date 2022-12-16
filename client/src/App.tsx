258fd99ba0862820bc6be23bc6a8dbe2011e18c3
raylu15742cs <raylucs15742@gmail.com>
2022-12-15 16:06:21 -0800

    update forms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,25 @@
+import React, { useState} from "react";
 import './App.css'
 
 function App() {
+  const [title, setTitle] = useState("");
+
+
   return (
     <div className="App">
+      <form>
+        <label htmlFor="deck-title">Deck Title</label>
+        <input 
+          id="deck-title"
+          value = {title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+            // Save what is typed
+            {
+              setTitle(e.target.value)
+            }
+          }
+        />
+      </form>
     </div>
   )
 }
