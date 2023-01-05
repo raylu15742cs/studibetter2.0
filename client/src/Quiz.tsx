ea309f7834578e8e41d0083205242c824cd5366a
raylu15742cs <raylucs15742@gmail.com>
2023-01-05 12:03:18 -0800

    update quiz

--- client/src/Quiz.tsx
+++ client/src/Quiz.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
 export default function Quiz() {
     const [quizcount, setQuizcount] = useState<number>(0)
+    const [score , setScore] = useState<number>(0)
 }
