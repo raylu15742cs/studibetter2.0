c4c3365e5fecee672fe67170fac604558a943306
raylu15742cs <raylucs15742@gmail.com>
2023-01-05 17:19:55 -0800

    add link to quiz

--- client/src/Quiz.tsx
+++ client/src/Quiz.tsx
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from "react";
+import { Link, useParams} from "react-router-dom";
 import Header from "./Header";
 export default function Quiz() {
     const [quizcount, setQuizcount] = useState<number>(0)
     const [score , setScore] = useState<number>(0)
+    
 
     return (
         <div>
