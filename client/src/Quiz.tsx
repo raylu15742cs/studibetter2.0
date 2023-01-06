ff7f42db2c963db12f3aa82cf6b46b3e892f2a25
raylu15742cs <raylucs15742@gmail.com>
2023-01-05 22:55:40 -0800

    update quiz

--- client/src/Quiz.tsx
+++ client/src/Quiz.tsx
@@ -1,15 +1,29 @@
 import React, {useState, useEffect} from "react";
 import { Link, useParams} from "react-router-dom";
+import { getCards, TCard } from "./api/cardHandler";
+import { TTopic } from "./api/topicHandler";
 import Header from "./Header";
 export default function Quiz() {
     const [quizcount, setQuizcount] = useState<number>(0)
     const [score , setScore] = useState<number>(0)
+    const [cards, setCards] = useState<TCard[]>([])
+    const [topic, setTopic] = useState<TTopic>()
     let { topicId } = useParams();
+
+    useEffect(() => {
+    async function fetchCards() {
+      if(!topicId) return;
+      const newCard = await getCards(topicId);
+      setCards(newCard.cards);
+      setTopic(newCard.topics);
+    }
+    fetchCards();
+  }, [topicId , cards])
 
     return (
         <div>
             <Header />
-            <h1>{topicId}</h1>
+            <h1>{topic?.title}</h1>
             <h3>{quizcount} {score}</h3>
         </div>
     )
