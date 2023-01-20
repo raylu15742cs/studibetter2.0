76e5e0f2a3ef60c827fdbce74c10b92437edb938
raylu15742cs <raylucs15742@gmail.com>
2023-01-20 13:52:31 -0800

    update quiz

--- client/src/Quiz.tsx
+++ client/src/Quiz.tsx
@@ -4,31 +4,7 @@ import { getCards, TCard } from "./api/cardHandler";
 import { TTopic } from "./api/topicHandler";
 import Header from "./Header";
 export default function Quiz() {
-    const [quizcount, setQuizcount] = useState<number>(0)
-    const [score , setScore] = useState<number>(0)
-    const [cards, setCards] = useState<TCard[]>([])
-    const [topic, setTopic] = useState<TTopic>()
-    let { topicId } = useParams();
-    const [status, setStatus] = useState("")
-    async function checkLength() {
-      if (cards.length > 10) {
-        setStatus("Big")
-      } else {
-        setStatus("Small")
-      }
-    }
-
-    useEffect(() => {
-    async function fetchCards() {
-      if(!topicId) return;
-      const newCard = await getCards(topicId);
-      setCards(newCard.cards);
-      setTopic(newCard.topics);
-    }
-    fetchCards();
-    checkLength();
-  }, [topicId , cards])
-
+  
     return (
         <div>
             <Header />
