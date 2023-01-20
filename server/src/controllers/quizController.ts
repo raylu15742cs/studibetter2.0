5cfffbf6f98a722b298941c2d0d5c3a319070d8e
raylu15742cs <raylucs15742@gmail.com>
2023-01-20 14:45:30 -0800

    update quiz

--- server/src/controllers/quizController.ts
+++ server/src/controllers/quizController.ts
@@ -8,7 +8,7 @@ export async function createQuiz(req: Request, res: Response) {
 export async function updateScore(req: Request, res: Response) {
     
 }
-
+// Function that handles each selection
 export async function checkSelection(req: Request, res: Response) {
     
 }
